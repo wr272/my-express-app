0f69bd6b34ac98c7c25b5dbda62221debacf50af
Patcurley1 <101998702+Patcurley1@users.noreply.github.com>
2022-05-01 21:14:22 -0400

    Deploy partial

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+// this is the "routes/index.js" file...
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//...
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
 });
 
-module.exports = router;
+/* GET hello page. */
+router.get('/hello', function(req, res, next) {
+  console.log("URL PARAMS:", req.query)
+
+  var name = req.query.name || "World" // double pipes is an OR operator that allows us to use a default value if the url params are null / not specified
+
+  var message = "Hello, " + name
+
+  // passing a variable called "message" to the page (referencing the object's key name)
+  res.render('hello', {message: message});
+});
+
+//...
