3bb5d1f656e28b3de92b5c68b8a52cfcf588d84c
Patcurley1 <101998702+Patcurley1@users.noreply.github.com>
2022-05-03 11:07:33 -0400

    Flash

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressLayouts = require('express-ejs-layouts'); // around line 6
 var indexRouter = require('./routes/index');
+var session = require('express-session') // around line 9
+var flash = require('express-flash-messages') // around line 10
 var usersRouter = require('./routes/users');
 var stocksRouter = require('./routes/stocks') // around line 12
 
+
 var app = express();
+var SESSION_SECRET = process.env.SESSION_SECRET || "super secret" // around line 16 (before app initialization)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,6 +28,15 @@ app.use(expressLayouts); // around new line 22
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/stocks', stocksRouter) // around line 29
+//around line 30 (after cookie parser and express layouts stuff)
+app.use(session({
+  cookie: { maxAge: 60000},
+  secret: SESSION_SECRET,
+  name: 'stocks-app-session',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
