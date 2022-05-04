066b017d952ff8ad00ebc7444ff242b95a3c6360
Patcurley1 <101998702+Patcurley1@users.noreply.github.com>
2022-05-04 10:41:20 -0400

    pushhhhhhh fix flash

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressLayouts = require('express-ejs-layouts'); // around line 6
-var indexRouter = require('./routes/index');
 var session = require('express-session') // around line 9
 var flash = require('express-flash-messages') // around line 10
+
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var stocksRouter = require('./routes/stocks') // around line 12
+var productsRouter = require('./routes/products'); // around line 15
 
+var SESSION_SECRET = process.env.SESSION_SECRET || "super secret" // around line 16 (before app initialization)
 
 var app = express();
-var productsRouter = require('./routes/products'); // around line 15
-var SESSION_SECRET = process.env.SESSION_SECRET || "super secret" // around line 16 (before app initialization)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -26,9 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(expressLayouts); // around new line 22
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/stocks', stocksRouter) // around line 29
+
 //around line 30 (after cookie parser and express layouts stuff)
 app.use(session({
   cookie: { maxAge: 60000},
@@ -39,7 +38,12 @@ app.use(session({
 }));
 app.use(flash())
 
-app.use('/products', productsRouter); // around line 43
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/stocks', stocksRouter) // around line 29
+app.use('/', productsRouter); // around line 43
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
